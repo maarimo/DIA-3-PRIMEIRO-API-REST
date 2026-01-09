1c5fe129ff7700b06e4be2ca208cb67bcfd91701
Matheus Amorim <abmm1234@hotmail.com>
2026-01-09 09:04:00 -0300

    FIRST COMMIT

---
+++ primeiro-api-rest/primeiro-api-rest/src/main/java/com/example/primeiro_api_rest/PrimeiroApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.primeiro_api_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroApiRestApplication.class, args);
+	}
+
+}
---
+++ primeiro-api-rest/primeiro-api-rest/src/test/java/com/example/primeiro_api_rest/PrimeiroApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.primeiro_api_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
